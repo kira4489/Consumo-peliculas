30bb0c474a3a1fd70a2dd4125494e2bcd4c150f8
jhonatanquirama <quiramapino47@gmail.com>
2020-09-12 11:31:43 -0500

    actualizaciones3

--- src/context/DatosUsuario.jsx
+++ src/context/DatosUsuario.jsx
@@ -2,9 +2,9 @@ import  React from 'react'
 
 const DatosUsuario = React.createContext({
     idUser : 87,
-    userName : "omesa",
-    fullName : "Oscar Mesa",
-    lastName : "Mesa"
+    userName : "kira4489",
+    fullName : "Jhonatan Quirama",
+    lastName : "Quirama"
 })
 
 export default DatosUsuario
